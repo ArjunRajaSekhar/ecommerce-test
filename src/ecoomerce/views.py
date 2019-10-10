8b358aa913ccd6e90bd730fe04208e14f5596436
ArjunRajaSekhar <rarjunsekhar@gmail.com>
2019-10-10 12:15:31 +0530

    cart app django sessions

--- src/ecoomerce/views.py
+++ src/ecoomerce/views.py
@@ -1,33 +1,34 @@
-from django.contrib.auth import authenticate, login , get_user_model
+from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from .forms import ContactForm,LoginForm,RegisterForm
+from django.shortcuts import render,redirect
+
+from .forms import ContactForm, LoginForm, RegisterForm
 
 def home_page(request):
-    context ={
-        "title":"HOME PAGE",
-        "content":"This is the homepage we're working on..",
-        
+    # print(request.session.get("first_name", "Unknown"))
+    # request.session['first_name']
+    context = {
+        "title":"Hello World!",
+        "content":" Welcome to the homepage.",
+
     }
     if request.user.is_authenticated():
-        context["premium_content"] = "THIS IS PREMIUM CONTENT" 
-    return render(request, "home_page.html" , context)
-
+        context["premium_content"] = "YEAHHHHHH"
+    return render(request, "home_page.html", context)
 
 def about_page(request):
-    context ={
-        "title":"ABOUT",
-        "content":"The details about the page..."
+    context = {
+        "title":"About Page",
+        "content":" Welcome to the about page."
     }
-    return render(request, "home_page.html" , context)
-
+    return render(request, "home_page.html", context)
 
 def contact_page(request):
     contact_form = ContactForm(request.POST or None)
-    context ={
-        "title":"CONTACT",
-        "content":"Get In Touch With Us.",
-        "form": contact_form
+    context = {
+        "title":"Contact",
+        "content":" Welcome to the contact page.",
+        "form": contact_form,
     }
     if contact_form.is_valid():
         print(contact_form.cleaned_data)
@@ -36,7 +37,7 @@ def contact_page(request):
     #     print(request.POST.get('fullname'))
     #     print(request.POST.get('email'))
     #     print(request.POST.get('content'))
-    return render(request, "contact/view.html" , context)
+    return render(request, "contact/view.html", context)
 
 
 def login_page(request):
@@ -44,25 +45,29 @@ def login_page(request):
     context = {
         "form": form
     }
-    print("user logged in")
-    print(request.user.is_authenticated())
+    print("User logged in")
+    #print(request.user.is_authenticated())
     if form.is_valid():
         print(form.cleaned_data)
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password")
-        user =authenticate(request ,username=username ,password=password)
-        print(request.user.is_authenticated())
+        username  = form.cleaned_data.get("username")
+        password  = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        #print(request.user.is_authenticated())
         if user is not None:
-            print(request.user.is_authenticated())
+            #print(request.user.is_authenticated())
             login(request, user)
+            # Redirect to a success page.
+            #context['form'] = LoginForm()
             return redirect("/")
         else:
+            # Return an 'invalid login' error message.
             print("Error")
-    return render(request, "auth/login.html",context)
+
+    return render(request, "auth/login.html", context)
 
 
-
-User=get_user_model()
+User = get_user_model()
 def register_page(request):
     form = RegisterForm(request.POST or None)
     context = {
@@ -70,12 +75,38 @@ def register_page(request):
     }
     if form.is_valid():
         print(form.cleaned_data)
-        username = form.cleaned_data.get("username")
-        email = form.cleaned_data.get("email")
-        password = form.cleaned_data.get("password")
-        new_user = User.objects.create_user(username,email,password)
+        username  = form.cleaned_data.get("username")
+        email  = form.cleaned_data.get("email")
+        password  = form.cleaned_data.get("password")
+        new_user  = User.objects.create_user(username, email, password)
         print(new_user)
-    return render(request, "auth/register.html",context)
+
+    return render(request, "auth/register.html", context)
 
 
-    
+
+
+def home_page_old(request):
+    html_ = """
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+      </head>
+      <body>
+        <div class='text-center'>
+            <h1>Hello, world!</h1>
+        </div>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+      </body>
+    </html>
+    """
+    return HttpResponse(html_)
